8b1390f51a0f1ddf58e49654c2d43a83f2e7a721
abelarcos <abel302025@hotmail.com>
2025-11-04 15:23:52 -0600

    cambios en el vite para produccion

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+/* import { defineConfig } from 'vite'; ANTES LOCAL 
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
@@ -6,6 +6,33 @@ export default defineConfig({
     plugins: [
         laravel({
             input: 'resources/js/app.js',
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+});*/
+
+//PRODUCCION
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    base: '/StyloSoft/', // <- importante para subdirectorio
+    plugins: [
+        laravel({
+            input: [
+                'resources/js/app.js',
+                // opcional: si quieres compilar CSS separado
+                // 'resources/css/app.css',
+            ],
             refresh: true,
         }),
         vue({
